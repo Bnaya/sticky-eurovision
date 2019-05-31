cf23d89c0a82ba2c74ef3fa5acde67ffbb7ca483
Bnaya Peretz <me@bnaya.net>
2019-05-31 10:11:58 +0300

    Add nav

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import appStyles from "./App.module.css";
+import { Link, Router, RouteComponentProps } from "@reach/router";
+import { hot } from "react-hot-loader/root";
+import { EurovisionTable } from "./EurovisionTable/EurovisionTable";
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={appStyles.App}>
+      <nav className={appStyles.nav}>
+        <ul>
+          <li>
+            <Link to="no-frozen-rows-columns">No Frozen Rows Columns</Link>
+          </li>
+          <li>
+            <Link to="/sticky-inline-block">
+              Using sticky + inline block layout
+            </Link>
+          </li>
+          <li>
+            <a
+              href="https://github.com/bvaughn/react-window/compare/master...Bnaya:iterations?expand=1#diff-24a00237a136050a93a551637894b705R283"
+              target="for-external-links"
+              rel="noopener noreferrer"
+            >
+              the react-window fork
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://github.com/bvaughn/react-window/issues/98"
+              target="for-external-links"
+              rel="noopener noreferrer"
+            >
+              The Sad Issue :(
+            </a>
+          </li>
+        </ul>
+      </nav>
+      <Router className={appStyles.content}>
+        <RouterPage
+          routeComponent={EurovisionTable}
+          path="/sticky-inline-block"
+        />
+      </Router>
     </div>
   );
-}
+};
 
-export default App;
+const RouterPage = (
+  props: { routeComponent: React.ComponentType<any> } & RouteComponentProps
+) => <props.routeComponent />;
+
+const HotApp: React.ComponentType<any> = hot(App);
+
+export { HotApp };
--- src/index.tsx
+++ src/index.tsx
@@ -4,9 +4,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import * as serviceWorker from "./serviceWorker";
-import { EurovisionTableHot } from "./EurovisionTable/EurovisionTable";
+import { HotApp } from "./App";
 
-ReactDOM.render(<EurovisionTableHot />, document.getElementById("root"));
+ReactDOM.render(<HotApp />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
